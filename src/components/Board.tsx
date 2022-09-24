a3d24e262c74df771834301cf9ae1020b84d4a09
hhw12409 <sksk2425@naver.com>
2022-09-25 01:49:06 +0900

    Feat: Board and DraggableCard

---
+++ src/components/Board.tsx
@@ -0,0 +1,96 @@
+import { useForm } from 'react-hook-form';
+import { Droppable } from 'react-beautiful-dnd';
+import DraggableCard from './DraggableCard';
+import styled from 'styled-components';
+import { ITodo, toDoState } from '../atoms';
+import { useSetRecoilState } from 'recoil';
+
+interface IBoardProps {
+  toDos: ITodo[];
+  boardId: string;
+}
+
+interface IForm {
+  toDo: string;
+}
+
+const Board = ({ toDos, boardId }: IBoardProps) => {
+  const setToDos = useSetRecoilState(toDoState);
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const onValid = ({ toDo }: IForm) => {
+    const newToDo = {
+      id: Date.now(),
+      text: toDo,
+    };
+    setToDos((allBoards) => {
+      return {
+        ...allBoards,
+        [boardId]: [...allBoards[boardId], newToDo],
+      };
+    });
+    setValue('toDo', '');
+  };
+  return (
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('toDo', { required: true })}
+          type="text"
+          placeholder={`Add task on ${boardId.toUpperCase()}`}
+        />
+      </Form>
+      <Droppable droppableId={boardId}>
+        {(provided, info) => (
+          <Area
+            isDraggingOver={info.isDraggingOver}
+            isDraggingFromThis={Boolean(info.draggingFromThisWith)}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {toDos.map((toDo, index) => (
+              <DraggableCard key={toDo.id} index={index} toDoId={toDo.id} toDoText={toDo.text} />
+            ))}
+            {provided.placeholder}
+          </Area>
+        )}
+      </Droppable>
+    </Wrapper>
+  );
+};
+
+export default Board;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  padding-top: 10px;
+  border-radius: 5px;
+  min-height: 300px;
+  background-color: ${(props) => props.theme.boardColor};
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  font-size: 18px;
+`;
+
+interface IAreaProps {
+  isDraggingFromThis: boolean;
+  isDraggingOver: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver ? '#dfe6e9' : props.isDraggingFromThis ? '#b2bec3' : 'transparent'};
+  flex: 1;
+  transition: background-color 0.3s ease-in-out;
+  padding: 20px;
+`;
+
+const Form = styled.form`
+  width: 100%;
+`;
